8ad3e1d4024ac90d8deb55fba2305b2ee696b5b6
Angel Moreno <moreno.angel71@gmail.com>
2022-10-06 19:02:33 -0700

    Subsetting DF by categories and players over 180 points

--- dataframes.R
+++ dataframes.R
@@ -1,4 +1,9 @@
-#Import datasets for each year that I'll be reviewing
+# Import libraries
+
+library(tidyverse)
+library(readr)
+
+# Import datasets for each year that I'll be reviewing
 
 fantasy2012 <- read_csv('https://raw.githubusercontent.com/fantasydatapros/data/master/yearly/2012.csv')
 fantasy2012$year <- rep(2012,nrow(fantasy2012))
@@ -39,3 +44,68 @@ fantasy2021 <- read_csv('https://raw.githubusercontent.com/fantasydatapros/data/
 fantasy2021$year <- rep(2021,nrow(fantasy2021))
 fantasy2021 <- fantasy2021[,-c(1)]
 
+# Bind each year together to make one master dataframe
+
+fullfantasy <- fantasy2012 %>% 
+  bind_rows(fantasy2013, fantasy2014, fantasy2015, fantasy2016, fantasy2017, fantasy2018, fantasy2019, fantasy2020, fantasy2021) %>% 
+  filter(FantasyPoints > 180)
+fullfantasy
+
+
+# Subsetting DF into positions
+
+fantasyRBs <- fullfantasy %>% 
+  filter(Pos == "RB")
+
+fantasyWRs <- fullfantasy %>% 
+  filter(Pos == "WR")
+
+fantasyQBs <- fullfantasy %>% 
+  filter(Pos == "QB")
+
+
+# Add columns to RB DF to categorize points based on rushing and receiving
+
+fantasyRBs <- fantasyRBs %>% 
+  mutate(RushingFantasyPoints = (RushingYds/10) + (RushingTD*6) - Fumbles, 
+         ReceivingFantasyPoints = Rec + (ReceivingYds/10) + (ReceivingTD*6))
+fantasyRBs
+
+
+#summary statistics for all positions
+
+summaryALL <- fullfantasy %>% 
+  group_by(Pos,year) %>% 
+  summarize(count = n(), mean_points = mean(FantasyPoints), median_points = median(FantasyPoints), prop_over_180 = mean(FantasyPoints > 180), total_over_180 = sum(FantasyPoints > 180))
+summaryALL
+
+
+#Summary statistics for each positon
+
+summaryRBs <- fantasyRBs %>% 
+  group_by(year) %>% 
+  summarize(count = n(), mean = mean(FantasyPoints), median = median(FantasyPoints))
+
+summaryWRs <- fantasyWRs %>% 
+  group_by(year) %>% 
+  summarize(count = n(), mean = mean(FantasyPoints), median = median(FantasyPoints))
+
+summaryQBs <- fantasyQBs %>% 
+  group_by(year) %>% 
+  summarize(count = n(), mean = mean(FantasyPoints), median = median(FantasyPoints))
+
+#Subset all players over 180 points in a year
+
+Over180 <- fullfantasy %>% 
+  filter(FantasyPoints >= 180, Pos %in% c("RB", "WR"))
+
+
+#Subset runningbacks to players over 180 points in a year
+
+RBsOver180 <- fantasyRBs %>% 
+  filter(FantasyPoints >= 180, Pos == "RB") %>% 
+  group_by(year) %>% 
+  summarise(RushingFantasyPoints = sum(RushingFantasyPoints), 
+            ReceivingFantasyPoints = sum(ReceivingFantasyPoints),
+            TotalPoints = sum(FantasyPoints)) %>% 
+  mutate(ProportionRushingPts = (RushingFantasyPoints/TotalPoints))
